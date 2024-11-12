4065eb4a70f85fce60293f5dac446dabf2f8f3a6
Antonio Carlos Souza Lino <antoniomotoe4plus@gmail.com>
2024-11-12 16:42:24 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -11,7 +11,7 @@ function carregar(){
         document.body.style.background = '#aa916e'
         txt.innerText = 'Bom Dia'
         
-     } else if (hora === 16 && min >= 20 && min <= 38) {
+     } else if (hora === 16 && min >= 20 && min <= 45) {
         igm.src = 'weed.png';
         document.body.style.background = '#016231';
         txt.innerText = 'Hora Especial';
