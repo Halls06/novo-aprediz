9a9c7942864d5d44af298381f9d0eeeccf0e12a9
Antonio Carlos Souza Lino <antoniomotoe4plus@gmail.com>
2024-11-12 18:39:16 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var igm = window.document.getElementById('imagem')
+    var txt = document.getElementById('txt')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes() 
+    
+    msg.innerHTML = `Agora São ${hora} : ${min} Horas.`
+   
+    if (hora >= 0 && hora < 12){
+        igm.src = 'manha.png'
+        document.body.style.background = '#aa916e'
+        txt.innerText = 'Bom Dia'
+   
+    }
+
+      else if (hora === 16 && min >= 20 && min <= 30) {
+        igm.src = 'weed.png';
+        document.body.style.background = '#016231';
+        txt.innerText = 'Hora Especial';
+    }
+    
+     else if (hora >= 12 && hora <= 18 && min < 1){
+        igm.src = 'tarde.png'
+        document.body.style.background = '#f1b07a'
+        txt.innerText = 'Boa Tarde'
+     }
+    
+   
+    else{
+        igm.src = 'noite.png'
+        document.body.style.background = '#224671'
+        txt.innerHTML = 'Boa Noite'
+    }
+
+}
